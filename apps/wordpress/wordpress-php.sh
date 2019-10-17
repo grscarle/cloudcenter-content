57be34fbdadfb630a7256d1420825e6ee86405c3
Greg Scarlett <grscarle@cisco.com>
2019-10-17 13:40:20 +1100

    Log messages

--- apps/wordpress/wordpress-php.sh
+++ apps/wordpress/wordpress-php.sh
@@ -7,8 +7,11 @@
 
 
 cd /tmp
+agentSendLogMessage "Installing REMI Repo"
 sudo wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 sudo rpm -Uvh remi-release-6.rpm 
 sudo yum install yum-utils -y
 sudo yum-config-manager --enable remi-php56
+agentSendLogMessage "Installing PHP 5.6"
 sudo yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo -y
+agentSendLogMessage "PHP 5.6 Installed"
